28a361247697cfcd053c3f97551334b009d6d30d
Riedel <riedeln@charite.de>
2017-08-14 17:34:36 +0200

    Now includes JIF dataset and Sherpa/Romeo API information. Detailed changes:
        Journal_Whitelist_Data_Merging.R:
         - DOAJ dataset is now filtered for "English" or "German" as only languages, subject is filtered to contain "Medicine"
         - journal impact factor dataset is loaded and matched with the doaj journal titles (exact match, to lower characters), added as extra column
         - create extra column is_PMC_listed, testing if 'Journal title.pmc' is not empty - right now only select journals with 'yes'
         - Sherpa/Romeo API is used to request the 'romeocolor' for each journal issn and additional romeo_colour column is added
         - romeo_colors are:
             green  (can archive pre and post-print of publishers PDF)
             blue   (can archive post-print of publishers PDF)
             yellow (can archive pre-print of publishers PDF)
             white  (archiving not formally supported)
             grey   (unknown status)
         - finally columns are rearranged and final table is saved to csv-file
    
        Journal_Whitelist_functions.R:
         - small helper functions
         - logical_to_yes_no() transforms logical vector to character "Yes"/"No" vector
         - getISSN() selects one ISSN from pISSN or eISSN (because often only one is given)
         - get_lowest_color() picks lowest romeo colour if for one journals several publishers are given with different colors

--- Journal_Whitelist_Data_Merging.R
+++ Journal_Whitelist_Data_Merging.R
@@ -1,4 +1,8 @@
+source('T:\\Dokumente\\Projekte\\Open Access Journals\\Journal Whitelist\\R-Code\\Journal_Whitelist_functions.R')
 library(tidyverse)
+library(XML)
+
+romeo_api_key <- "4EDDuZOh6GE"
 
 #----------------------------------------------------------------------------------------------------------------------------
 # DOAJ dataset
@@ -12,11 +16,13 @@ useful_cols_doaj <- c('Journal title', 'Journal URL', 'Journal ISSN (print versi
                  'APC amount', 'Currency', "Journal article submission fee",
                  "Submission fee URL", "Submission fee amount", "Submission fee currency",
                  "Full text language", "Average number of weeks between submission and publication",
-                 "Journal license", "Author holds copyright without restrictions")
+                 "Journal license", "Author holds copyright without restrictions", "Subjects")
 
 
 doaj_data <- doaj_data %>% select(one_of(useful_cols_doaj))
-doaj_data <- doaj_data %>% filter(grepl("English|German", `Full text language`))
+doaj_data <- doaj_data %>% 
+  filter(`Full text language` == "English" | `Full text language` == "German") %>% #English only journals
+  filter(grepl("Medicine", `Subjects`)) #Which categories to use?
 
 #rename some columns
 doaj_data <- doaj_data %>% rename(pISSN = `Journal ISSN (print version)`)
@@ -36,6 +42,19 @@ pmc_data <- pmc_data %>% select(one_of(useful_cols_pmc))
 
 
 #----------------------------------------------------------------------------------------------------------------------------
+# journal impact factor dataset - not clear if we will need it
+#----------------------------------------------------------------------------------------------------------------------------
+
+jif_data <- read_csv('T:\\Dokumente\\Projekte\\Open Access Journals\\Journal Whitelist\\Journal_Impact_Factor.csv')
+
+useful_cols_jif <- c("Full Journal Title", "Journal Impact Factor")
+
+jif_data <- jif_data %>% select(one_of(useful_cols_jif))
+jif_data <- jif_data %>%
+  mutate(`Full Journal Title` = tolower(`Full Journal Title`))
+
+
+#----------------------------------------------------------------------------------------------------------------------------
 # join datasets
 #----------------------------------------------------------------------------------------------------------------------------
 
@@ -52,4 +71,70 @@ joined_data_eISSN <- doaj_data %>%
 #combine both join versions and remove duplicates
 joined_data <- joined_data_pISSN %>%
   union(joined_data_eISSN)
+
+#add column is_PMC_listed by computing which rows have journal title.pmc entry
+joined_data <- joined_data %>%
+  mutate(is_PMC_listed = !is.na(`Journal title.pmc`)) %>%
+  mutate(is_PMC_listed = logical_to_yes_no(is_PMC_listed)) 
   
+#only select PMC-listed journals?
+joined_data <- joined_data %>%
+  filter(is_PMC_listed == "yes")
+
+#add journal impact factor column
+joined_data <- joined_data %>%
+  mutate(`Journal title match` = tolower(`Journal title.doaj`)) %>%
+  left_join(jif_data, by = c("Journal title match" = "Full Journal Title")) %>%
+  select(-`Journal title match`)
+  
+
+#----------------------------------------------------------------------------------------------------------------------------
+# retrieve sherpa/romeo info and add to data table
+#----------------------------------------------------------------------------------------------------------------------------
+
+romeo_color_list <- rep("", dim(joined_data)[1])
+for(i in 1:dim(joined_data)[1])
+{
+  #retrieve romeo color for given issn
+  issn <- getISSN(joined_data[["pISSN"]][i], joined_data[["eISSN"]][i])
+  sherpa_entry <- readLines(paste0("http://www.sherpa.ac.uk/romeo/api29.php?issn=", issn, "&ak=", romeo_api_key))
+  sherpa_entry <- xmlParse(sherpa_entry)
+  romeo_colour <- xpathSApply(sherpa_entry, "//romeocolour", fun = xmlValue)
+  
+  #handle cases with no or several entries
+  if(length(romeo_colour) == 0) {
+    romeo_colour <- ""
+  } else {
+    romeo_colour <- get_lowest_color(romeo_colour)
+  }
+  
+  #fill list
+  print(paste0("i: ", i, " romeo_colour: ", romeo_colour))
+  romeo_color_list[i] <- romeo_colour[1]
+}
+
+#add romeo color as new column
+joined_data <- joined_data %>%
+  add_column(romeo_color_list)
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+# save current version of spreadsheet
+#----------------------------------------------------------------------------------------------------------------------------
+
+#rearrange columns
+final_col <- c('Journal title.doaj', "is_PMC_listed", "Journal Impact Factor",
+               "romeo_color_list", "Free access", "Open access", "Journal license",
+               "Author holds copyright without restrictions", "Subjects",
+               'Journal article processing charges (APCs)', 'APC information URL', 
+               'APC amount', 'Currency', "Journal article submission fee",
+               "Submission fee URL", "Submission fee amount", "Submission fee currency",
+               "Average number of weeks between submission and publication",
+               'Journal URL', 'Publisher.doaj', "pISSN", "eISSN")
+joined_data <- joined_data %>% 
+  select(one_of(final_col)) %>%
+  rename(`Journal title` = `Journal title.doaj`) %>% 
+  arrange(`Journal title`)
+
+save_filename <- 'T:\\Dokumente\\Projekte\\Open Access Journals\\Journal Whitelist\\Journal_Whitelist_Table.csv'
+write_csv(joined_data, save_filename)
---
+++ Journal_Whitelist_functions.R
@@ -0,0 +1,31 @@
+#transforms logical columns to yes/no columns
+logical_to_yes_no <- function(logic_vec) 
+{
+  yes_no <- c("no", "yes")
+  return(yes_no[logic_vec + 1])
+}
+
+
+#sometimes only pISSN or eISSN given, choose one
+getISSN <- function(pISSN, eISSN)
+{
+  if(!is.na(pISSN)) {
+    ISSN <- pISSN
+  } else {
+    ISSN <- eISSN
+  }
+  
+  return(ISSN)
+}
+
+
+#in some cases several publishers are given for one journal - then pick lowest romeo col
+get_lowest_color <- function(romeo_col_vec)
+{
+  color_list <- c("white", "gray", "yellow", "blue", "green")
+  lowest_col_idx <- which(color_list %in% romeo_colour)[1]
+  lowest_col <- color_list[lowest_col_idx]
+  
+  return(lowest_col)
+}
+
