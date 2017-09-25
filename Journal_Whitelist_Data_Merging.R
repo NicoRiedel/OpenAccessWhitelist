bfd22a6eca12b31e1388301e46191f1a21a0fb9c
Riedel <riedeln@charite.de>
2017-09-25 09:56:21 +0200

    Deleted Sherpa-Romeo Pre-/Post-/Publ-PDF archiving columns and all associated code lines. For the APC columns, split combined column into value and currency column again and added column with APCs converted to Euro. Also added Yes/No column if converted amount is below 2000 EUR.

--- Journal_Whitelist_Data_Merging.R
+++ Journal_Whitelist_Data_Merging.R
@@ -2,7 +2,9 @@ source('T:\\Dokumente\\Projekte\\Open Access Journals\\Journal Whitelist\\R-Code
 library(tidyverse)
 library(XML)
 
-romeo_api_key <- "4EDDuZOh6GE"
+#currency excange rates
+dollar_to_euro <- 0.8392
+pound_to_euro <- 1.1369
 
 #----------------------------------------------------------------------------------------------------------------------------
 # DOAJ dataset
@@ -30,11 +32,21 @@ doaj_data <- doaj_data %>%
 #modify and merge APC columns to condese information in one column
 doaj_data <- doaj_data %>% 
   mutate(Currency = substr(Currency, 1, 3)) %>% 
-  mutate(APC = paste(`APC amount`, `Currency`))
+  mutate(APC = `APC amount`)
 
 #value replacement for subset of rows is not properly implemented in dplyr as simple solution, so use usual R way
 noAPC_idx <- which(doaj_data["Journal article processing charges (APCs)"] != "Yes")
 doaj_data[noAPC_idx, "APC"] <- doaj_data[noAPC_idx, "Journal article processing charges (APCs)"]
+
+#create column with APCs converted to EUR
+doaj_data <- doaj_data %>% add_column(`APC in EUR` = NA)
+doaj_data[which(doaj_data$Currency =="USD"), "APC in EUR"] <-
+  round(doaj_data[which(doaj_data$Currency =="USD"), "APC amount"] * dollar_to_euro)
+doaj_data[which(doaj_data$Currency =="GBP"), "APC in EUR"] <-
+  round(doaj_data[which(doaj_data$Currency =="GBP"), "APC amount"] * pound_to_euro)
+doaj_data[which(doaj_data$Currency =="EUR"), "APC in EUR"] <-
+  doaj_data[which(doaj_data$Currency =="EUR"), "APC amount"] 
+doaj_data <- doaj_data %>% mutate(`APC below 2000 EUR` = logical_to_yes_no(`APC in EUR` < 2000))
 
 
 #rename some columns
@@ -48,7 +60,7 @@ doaj_data <- doaj_data %>%
 doaj_data <- doaj_data %>% 
   select(-`Journal article submission fee`, -`Submission fee URL`, 
          -`Submission fee amount`, -`Submission fee currency`, 
-         -`APC drop`, -`APC amount`, -Currency)
+         -`APC drop`, -`APC amount`)
 
 #----------------------------------------------------------------------------------------------------------------------------
 # PMC dataset
@@ -109,85 +121,21 @@ joined_data <- joined_data %>%
   
 
 #----------------------------------------------------------------------------------------------------------------------------
-# retrieve sherpa/romeo info and add to data table
-#----------------------------------------------------------------------------------------------------------------------------
-
-pre_archiving <- rep("", dim(joined_data)[1])
-post_archiving <- rep("", dim(joined_data)[1])
-post_restriction <- rep("", dim(joined_data)[1])
-publ_archiving <- rep("", dim(joined_data)[1])
-for(i in 1:dim(joined_data)[1])
-{
-  #get sherpa entry for given issn
-  issn <- getISSN(joined_data[["pISSN"]][i], joined_data[["eISSN"]][i])
-  sherpa_entry <- readLines(paste0("http://www.sherpa.ac.uk/romeo/api29.php?issn=", issn, "&ak=", romeo_api_key))
-  sherpa_entry <- xmlParse(sherpa_entry)
-
-  #retrieve archiving rights - only select first publisher entry, as there are sometimes conflicting results 
-  #for different publishers and usually the first publisher is the current or most important one
-  pre_arch <- xpathSApply(sherpa_entry, "//publisher[1]//prearchiving", fun = xmlValue)
-  if(length(pre_arch) == 0) { pre_arch <- "" }#handle cases with no or several entries
-  
-  post_arch <- xpathSApply(sherpa_entry, "//publisher[1]//postarchiving", fun = xmlValue)
-  if(length(post_arch) == 0) { post_arch <- "" }
-  
-  publ_arch <- xpathSApply(sherpa_entry, "//publisher[1]//pdfarchiving", fun = xmlValue)
-  if(length(publ_arch) == 0) { publ_arch <- "" }
-  
-  #postprint restriction is different, need to extract timeframe from xml
-  post_restr <- xpathSApply(sherpa_entry, "//publisher[1]//postprints//postrestriction", fun = xmlValue)
-  if(length(post_restr) == 0) { 
-    post_restr <- "" 
-  } else {
-    if(length(post_restr) == 1) {
-      post_restr <- extract_post_restr(post_restr)
-    } else {
-      post_restr <- paste((sapply(post_restr, extract_post_restr)), collapse = "; ") 
-    }
-  }
-
-  #fill list
-  print(paste0("i: ", i, " pre_arch: ", pre_arch, " post_arch: ", post_arch, " publ_arch: ", publ_arch, " post_restr: ", post_restr))
-  pre_archiving[i] <- pre_arch
-  post_archiving[i] <- post_arch
-  publ_archiving[i] <- publ_arch
-  post_restriction[i] <- post_restr
-  
-}
-
-#paste post restriction entry to post_archiving for the few cases where it is not empty
-hasRestr_idx <- which(post_restriction != "")
-post_archiving[hasRestr_idx] <- paste(post_archiving[hasRestr_idx], post_restriction[hasRestr_idx], sep = ": ")
-
-
-#add romeo color and is_hybrid as new column
-joined_data <- joined_data %>%
-  add_column(pre_archiving) %>%
-  add_column(post_archiving) %>%
-  add_column(publ_archiving)
-
-#rename new columns
-joined_data <- joined_data %>% 
-  rename(`Can archive pre-print` = pre_archiving) %>%
-  rename(`Can archive post-print` = post_archiving) %>%
-  rename(`Can archive publisher's version/PDF` = publ_archiving)
-
-#----------------------------------------------------------------------------------------------------------------------------
 # save current version of spreadsheet
 #----------------------------------------------------------------------------------------------------------------------------
 
 #rearrange columns
 final_col <- c('Journal title.doaj',  
-               'Journal article processing charges (APCs)', 'APC information URL',
+               'Journal article processing charges (APCs)', 'Currency',
+               'APC in EUR', 'APC below 2000 EUR',
+               'APC information URL', "Journal license", 
                "Average number of weeks between submission and publication",
-               "Can archive pre-print", "Can archive post-print",
-               "Can archive publisher's version/PDF", "Journal license", "Subjects", 
-               'Journal URL', 'Publisher.doaj', "pISSN", "eISSN")
+               "Subjects", 'Journal URL', 'Publisher.doaj', "pISSN", "eISSN")
 joined_data <- joined_data %>% 
   select(one_of(final_col)) %>%
   rename(`Journal title` = `Journal title.doaj`) %>% 
   rename(Publisher = `Publisher.doaj`) %>% 
-  arrange(`Journal title`)
+  arrange(Subjects)
 
 save_filename <- 'T:\\Dokumente\\Projekte\\Open Access Journals\\Journal Whitelist\\Journal_Whitelist_Table.csv'
 write_csv(joined_data, save_filename)
